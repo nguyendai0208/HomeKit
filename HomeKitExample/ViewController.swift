b5af1da9d88f18e5b9c9617fe4cb8916a04fca45
Nguyen Quang Dai <dai.nguyen@bstarsolutions.com>
2022-02-22 10:58:41 +0700

    Initial Commit

---
+++ HomeKitExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeKitExample
+//
+//  Created by Neo Nguyen on 22/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
